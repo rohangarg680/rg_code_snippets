44ea167a392e1c4b0e85268675831ab192d5d7e4
rohangarg680 <35104356+rohangarg680@users.noreply.github.com>
2023-06-21 17:56:34 +0530

    Update slowQueryLogging.js

--- slowQueryLogging.js
+++ slowQueryLogging.js
@@ -7,18 +7,18 @@ const exec = require('child_process').exec;
 //   password: '-HrDfKMS3o5Lz3Ml',
 //   database: 'db_sloth',
 // });
+var source = process.argv[1]  || `HIPPO`;
+var dbType = process.argv[2]  || `MASTER`;
+var taskCounter = 0;
 
 const mysql = require('mysql');
 const pool = mysql.createPool({
   connectionLimit: 150,
-  host: '172.31.10.103',
-  user: 'rohangarg',
-  password: '-HrDfKMS3o5Lz3Ml',
+  host: process.argv[3] || '172.31.10.103',
+  user: process.argv[4] || 'rohangarg',
+  password: process.argv[5]  || '-HrDfKMS3o5Lz3Ml',
   database: 'db_sloth',
 });
-
-var source = `HIPPO`;
-var dbType = `MASTER`;
 
 var time = new Date(new Date().getTime() - 60 * 1000)
   .toISOString()
@@ -28,13 +28,7 @@ pool.getConnection((err, connection) => {
   if (err) throw err;
   exec(`tail -n 1000 mysql-slow.log `, { maxBuffer: 1024 * 1000 }, function (error, stdout, stderr) {
     if (error) {
-      console.log(
-        'error>>>>>>>>>>>>>>>>>>>>>>>: ',
-        error,
-        stderr,
-        ':::::::::',
-        stdout
-      );
+      console.log('error>>>>>>>>: ',error,stderr,':::::::::',stdout);
       statusCode = error.code;
     } else {
       for (var query of stdout.split('Time:')) {
@@ -48,6 +42,7 @@ pool.getConnection((err, connection) => {
           var rowSent = query.match(/Rows_sent: (\d+)\s+/)[1];
           var rowExamined = query.match(/Rows_examined: (\d+)\s+/)[1];
           rawQuery = rawQuery[0].replace(';\n', '');
+          taskCounter++;
 		
           console.log({
             queryTime,
@@ -58,11 +53,14 @@ pool.getConnection((err, connection) => {
           });
 
           connection.query(
-            `INSERT INTO slow_queries (source, db_type, query_id, query, query_time, row_examined, row_sent) VALUES (
-              ?,?,?,?,?,?,?
-              )`,
-            [source, dbType, '1', rawQuery, queryTime, rowExamined, rowSent],
+            `INSERT INTO 
+	    	slow_queries 
+      		(source, db_type, query_id, query, query_time, row_examined, row_sent, lock_time) 
+	 	VALUES 
+   		(?,?,?,?,?,?,?,?) `,
+            [source, dbType, '1', rawQuery, queryTime, rowExamined, rowSent, lockTime],
             (error, results) => {
+	      taskCounter--;
               if (error) throw error;
               console.log(results);
             }
