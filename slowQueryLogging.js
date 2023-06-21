c1ba1ab007dd0ffec365b8a2d4feda6865e2d5cf
rohangarg680 <35104356+rohangarg680@users.noreply.github.com>
2023-06-21 17:02:20 +0530

    Update slowQueryLogging.js

--- slowQueryLogging.js
+++ slowQueryLogging.js
@@ -24,51 +24,54 @@ var time = new Date(new Date().getTime() - 60 * 1000)
   .toISOString()
   .substring(0, 16);
 
-exec(`tail -n 1000 mysql-slow.log `, {maxBuffer: 1024 * 1000} ,function (error, stdout, stderr) {
-  if (error) {
-    console.log(
-      'error>>>>>>>>>>>>>>>>>>>>>>>: ',
-      error,
-      stderr,
-      ':::::::::',
-      stdout
-    );
-    statusCode = error.code;
-  } else {
-    for (var query of stdout.split('Time:')) {
-      console.log('#######################',time, query);
-	    if(!query.includes(time)){console.log("query rejected>>>>>>"); continue;}
+pool.getConnection((err, connection) => {
+  if (err) throw err;
+  exec(`tail -n 1000 mysql-slow.log `, { maxBuffer: 1024 * 1000 }, function (error, stdout, stderr) {
+    if (error) {
+      console.log(
+        'error>>>>>>>>>>>>>>>>>>>>>>>: ',
+        error,
+        stderr,
+        ':::::::::',
+        stdout
+      );
+      statusCode = error.code;
+    } else {
+      for (var query of stdout.split('Time:')) {
+        console.log('#######################', time, query);
+        if (!query.includes(time)) { console.log("query rejected>>>>>>"); continue; }
 
-	    var rawQuery = query.match(/;([^;]*);/);
-      if (rawQuery && rawQuery[0]) {
-        var queryTime = query.match(/Query_time: (\d+\.\d+)\s+/)[1];
-        var lockTime = query.match(/Lock_time: (\d+\.\d+)\s+/)[1];
-        var rowSent = query.match(/Rows_sent: (\d+)\s+/)[1];
-        var rowExamined = query.match(/Rows_examined: (\d+)\s+/)[1];
-        rawQuery = rawQuery[0].replace(';\n', '');
-        console.log({
-          queryTime,
-          lockTime,
-          rowSent,
-          rowExamined,
-          rawQuery,
-        });
+        var rawQuery = query.match(/;([^;]*);/);
+        if (rawQuery && rawQuery[0]) {
+          var queryTime = query.match(/Query_time: (\d+\.\d+)\s+/)[1];
+          var lockTime = query.match(/Lock_time: (\d+\.\d+)\s+/)[1];
+          var rowSent = query.match(/Rows_sent: (\d+)\s+/)[1];
+          var rowExamined = query.match(/Rows_examined: (\d+)\s+/)[1];
+          rawQuery = rawQuery[0].replace(';\n', '');
+		
+          console.log({
+            queryTime,
+            lockTime,
+            rowSent,
+            rowExamined,
+            rawQuery,
+          });
 
-        pool.getConnection((err, connection) => {
-          if (err) throw err;
           connection.query(
             `INSERT INTO slow_queries (source, db_type, query_id, query, query_time, row_examined, row_sent) VALUES (
               ?,?,?,?,?,?,?
               )`,
             [source, dbType, '1', rawQuery, queryTime, rowExamined, rowSent],
             (error, results) => {
-              connection.release();
               if (error) throw error;
               console.log(results);
             }
           );
-        });
+
+        }
       }
     }
-  }
+  });
 });
+
+
